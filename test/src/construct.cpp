510a194a1dc23e5b56067ee5b40d8ec39a7abe57
Yankel Scialom (YSC) <yankel@scialom.org>
2019-01-08 15:31:38 +0100

    Fix unit test relying on UB.

--- test/src/construct.cpp
+++ test/src/construct.cpp
@@ -5,7 +5,15 @@
 // Expect matrix elements to be zero-initialized for trivial types
 TEST(construct, default_trivial_type)
 {
-    ysc::matrix<unsigned, 1> m;
+    /*
+     * Since reading uninitialized memory is UB, we can not rely solely on
+     * reading the value of a matrix<unsigned> element after default-init.
+     */
+    std::array<unsigned char, sizeof(unsigned)> non_zero_memory;
+    constexpr unsigned char non_zero = 0xC5;
+    non_zero_memory.fill(non_zero);
+
+    auto const& m = *new(non_zero_memory.data()) ysc::matrix<unsigned, 1>();
     ASSERT_EQ(m(0), 0);
 }
 
@@ -14,13 +22,15 @@ TEST(construct, default_user_defined_type)
 {
     struct user_defined : ysc::test::SideEffect<>
     { user_defined() { trigger(); } };
-    ysc::matrix<user_defined, 1> m;
+    ysc::matrix<user_defined, 1> const m;
     ASSERT_TRUE(m(0).triggered());
 }
 
-
-int main(int argc, char** argv)
+// Expect matrix elements to be default-initialized for user-defined types
+TEST(construct, value_user_defined_type)
 {
-    testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+    struct user_defined : ysc::test::SideEffect<>
+    { user_defined() { trigger(); } };
+    ysc::matrix<user_defined, 1> const m;
+    ASSERT_TRUE(m(0).triggered());
 }
