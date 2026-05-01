4c5c6fec653865c112d4d9a26b3e727c91e68de7
Yankel Scialom <yankel@scialom.org>
2026-05-02 01:39:00 +0200

    US-015 — Typedefs membres STL-compatibles (#9)
    Ajoute les dix alias de type standards (value_type, size_type,
    difference_type, reference, const_reference, pointer, const_pointer,
    iterator, const_iterator, reverse_iterator, const_reverse_iterator)
    dans ysc::matrix, débloquant US-016 (itérateurs).

---
+++ test/src/typedefs.cpp
@@ -0,0 +1,45 @@
+#include <concepts>
+#include <cstddef>
+#include <iterator>
+#include <gtest/gtest.h>
+#include "matrix.hpp"
+
+using M = ysc::matrix<int, 2, 3>;
+
+static_assert(std::same_as<M::value_type,             int>);
+static_assert(std::same_as<M::size_type,              std::size_t>);
+static_assert(std::same_as<M::difference_type,        std::ptrdiff_t>);
+static_assert(std::same_as<M::reference,              int&>);
+static_assert(std::same_as<M::const_reference,        const int&>);
+static_assert(std::same_as<M::pointer,                int*>);
+static_assert(std::same_as<M::const_pointer,          const int*>);
+static_assert(std::same_as<M::reverse_iterator,       std::reverse_iterator<M::iterator>>);
+static_assert(std::same_as<M::const_reverse_iterator, std::reverse_iterator<M::const_iterator>>);
+
+TEST(typedefs, value_type_is_int)
+{
+    static_assert(std::same_as<ysc::matrix<int, 3>::value_type, int>);
+    static_assert(std::same_as<ysc::matrix<double, 2, 2>::value_type, double>);
+}
+
+TEST(typedefs, size_type_and_difference_type)
+{
+    static_assert(std::same_as<M::size_type, std::size_t>);
+    static_assert(std::same_as<M::difference_type, std::ptrdiff_t>);
+}
+
+TEST(typedefs, reference_and_pointer)
+{
+    static_assert(std::same_as<M::reference,       int&>);
+    static_assert(std::same_as<M::const_reference, const int&>);
+    static_assert(std::same_as<M::pointer,         int*>);
+    static_assert(std::same_as<M::const_pointer,   const int*>);
+}
+
+TEST(typedefs, iterator_types_are_public)
+{
+    static_assert(std::same_as<M::iterator,               std::array<int, 6>::iterator>);
+    static_assert(std::same_as<M::const_iterator,         std::array<int, 6>::const_iterator>);
+    static_assert(std::same_as<M::reverse_iterator,       std::reverse_iterator<M::iterator>>);
+    static_assert(std::same_as<M::const_reverse_iterator, std::reverse_iterator<M::const_iterator>>);
+}
